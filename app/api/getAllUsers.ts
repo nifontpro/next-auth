88f5f585f6aea974f23c5ef359ea5542e68587bf
Zykov Artem <skaterpunisher@gmail.com>
2023-09-26 14:08:15 +0300

    remove and create new project

--- app/api/getAllUsers.ts
+++
@@ -1,7 +0,0 @@
-export function getAllUsers(): Promise<User[]> {
-  return fetch('http://localhost:8080/all',
-      // {cache: "force-cache"} // default, SSG (getStaticProps)
-      // {cache: "no-store"} // SSR (getServerSideProps)
-      // {next: {revalidate: 60}} // ISR
-  ).then(res => res.json())
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
-import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
-import Providers from "@/app/providers";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-    <Providers>
-      <body className={inter.className}>
-      {children}
-      </body>
-    </Providers>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
